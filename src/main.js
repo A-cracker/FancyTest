fbb7985a3edc7e52b19c320b1d49c4e82a79f7a2
A-cracker <1281590115@qq.com>
2021-06-26 19:14:33 +0800

    配置vuex

--- src/main.js
+++ src/main.js
@@ -3,15 +3,28 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import axios from 'axios'
+import Vuex from 'vuex'
 
-
+Vue.use(Vuex) 
 Vue.config.productionTip = false
 Vue.prototype.$ajax=axios
 
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
 
 new Vue({
+  store:store,
   vuetify,
   router,
   render: h => h(App)
 }).$mount('#app')
 
+         
