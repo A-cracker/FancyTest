ae4c6d200603bff84d1557d4f6bdf3307179ec60
A-cracker <1281590115@qq.com>
2021-08-14 16:57:24 +0800

    增加富文本编辑器

--- src/views/TheDetail.vue
+++ src/views/TheDetail.vue
@@ -8,17 +8,33 @@
         </v-toolbar-title>
       <v-spacer></v-spacer>
 
+    <the-ckeditor ref="editorRef"></the-ckeditor>
+    <button @click="setContent">设置内容</button>
+    <button @click="getContent">获取内容</button>
         <p>{{id}}</p>
     </div>
 </template>
 <script>
+import TheCkeditor from "@/components/TheCkeditor";
 export default{
     name:'TheDetail',
     props:['id','title'],
     data:()=>({
 
     }),
+    components:{
+      TheCkeditor
+    },
     mounted:{
+    },
+    methods: {
+      setContent(){
+        this.$refs.editorRef.setContent("<h1>这是ckEditor</h1>")
+      },
+
+      getContent(){
+        console.log(this.$refs.editorRef.getContent())
+      }
     }
 }
 </script>
